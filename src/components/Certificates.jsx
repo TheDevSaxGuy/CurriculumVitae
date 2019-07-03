2175b06eb502bee9f1ec790a73623039bb0c8647
johnnysax97 <juanitortz97@gmail.com>
2019-07-02 21:11:20 -0500

    Certificados, experiencia

---
+++ src/components/Certificates.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Certificates = ()=>(
+    <div className="Certificates">
+        <div className="Certificates-container">
+            <div className="Certificates-item">
+                <h3>Aptis</h3>
+                <p>Nivel C</p>
+            </div>
+        </div>
+    </div>
+);
+export default Certificates;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Experience = ()=>(
+    <div className="Experience">
+        <div className="Experience-container">
+            <div className="Experience-item">
+                <h3>Gaven</h3>
+                <p>Tecnico en Sistemas</p>
+            </div>
+        </div>
+    </div>
+);
+export default Experience;
