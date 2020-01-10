a31d642bc86a85e188b8d338af6d17e7429b45ea
johnnysax97 <juanitortz97@gmail.com>
2020-01-09 19:09:58 -0600

    Calling the API

--- src/Hooks/useGetData.js
+++ src/Hooks/useGetData.js
@@ -1,8 +1,11 @@
-import {useState, useEfect, useEffect} from 'react';
-
+import {useState, useEffect} from 'react';
+const api = 'https://us-central1-api-cv-2bf68.cloudfunctions.net/api';
 const useGetData = () =>{
     const [myData, setData] = useState([]);
     useEffect(()=>{
-        
-    });
+        fetch(api).then(response=>response.json()).then(data =>setData(data));
+    },[]);
+    return myData;
 } 
+
+export default useGetData;
--- src/containters/App.jsx
+++ src/containters/App.jsx
@@ -7,8 +7,11 @@ import Education from '../components/Education';
 import Experiencie from '../components/Experience';
 import Certificates from '../components/Certificates';
 import Skills from '../components/Skills';
+import useGetData from '../Hooks/useGetData';
 
 const App = () =>{
+    const data = useGetData();
+    console.log(data);
     return (
         <Main>
             <Sidebar>
