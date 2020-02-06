bec8d69c919ad15b67a524574139cb8a5e3004e6
Juan Antonio Ortiz <45924727+johnnysax97@users.noreply.github.com>
2020-02-05 22:05:43 -0600

    Ready to Prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output:{
@@ -31,7 +31,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./public/index.html",
             file:"./index.html"
-        })
+        }),
+        new FaviconsWebpackPlugin('./public/icono.png'),
         
     ]
 }
