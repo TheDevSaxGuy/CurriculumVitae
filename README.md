2cd02434c854d05849d34791a2211d76141feac7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-07 03:18:37 +0000

    Deploy johnnysax97/curriculumvitae to github.com/johnnysax97/curriculumvitae.git:master

--- README.md
+++
@@ -1,3 +0,0 @@
-# Curriculum vitae
-Single Page Application que contendra mi CV o portafolio.
-Desarrollado con Reactjs y Nodejs Siguiendo el curso de Facebook Developer Circle.
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containters/App';
-
-
-ReactDOM.render(<App />, document.getElementById('app'));
